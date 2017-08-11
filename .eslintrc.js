ea57c63802276c7199bee502f6742a24edcbda00
Maegan Womble <maegan@oseberg.io>
2017-08-11 15:08:21 -0500

    search bar container

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    "extends": "airbnb",
+    "rules": {
+        "react/jsx-filename-extension": 0,
+        "max-len": 0,
+        "react/prop-types": 0,
+        "react/prefer-stateless-function": 0,
+    }
+};
